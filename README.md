9d3103414aa63c831a0d27e5b28dddd86781d741
Nikhil Vemula <vsrinivasanikhil@gmail.com>
2021-10-24 15:50:53 +0530

    Update readme

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # My Todos
 
+## Description
+
 A react application with fire base backend to manage todos
+
+## Run
+
+```bash
+npm install
+npm run build
+npm run start
+```
+
+## Technologies used
+
+* React
+* Redux
+* Firebase
--- src/common/firebase.js
+++ src/common/firebase.js
@@ -5,13 +5,13 @@ import { auth as authui } from "firebaseui";
 import { updateUserState, userStateFetched } from "../redux/actions";
 
 const config = {
-  apiKey: "AIzaSyCw4gIrL9BkiZPqoy0HTNrxGXn30CCRff4",
-  authDomain: "my-todos-2ea97.firebaseapp.com",
-  projectId: "my-todos-2ea97",
-  storageBucket: "my-todos-2ea97.appspot.com",
-  messagingSenderId: 320712512916,
-  appId: "1:320712512916:web:1cf4d6f57a716e71b2ab81",
-  measurementId: "G-1V9RQ7WJMR",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: '',
+  appId: "",
+  measurementId: "",
 };
 
 firebase.initializeApp(config);
